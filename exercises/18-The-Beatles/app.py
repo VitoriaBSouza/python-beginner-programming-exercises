d3c64997210eec8ecf5f73ead8be7807fc2e9196
Vbarbosa <131600563+VitoriaBSouza@users.noreply.github.com>
2025-05-03 18:17:15 +0000

    completed all excercises

--- exercises/18-The-Beatles/app.py
+++ exercises/18-The-Beatles/app.py
@@ -1,2 +1,14 @@
 # ✅↓ Write your code here ↓✅
 
+def sing():
+    song = ""
+    for i in range(11):
+        if i == 4:
+            song += "there will be an answer, "
+        elif i == 10:
+            song += "whisper words of wisdom, let it be "
+        else:
+            song += "let it be, "
+    return song
+
+print(sing())
